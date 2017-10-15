996ed5c64f701384d45a92fa7462197a9ec93926
Matt Vogel <matt@portlandwebdev.com>
2017-10-14 19:36:03 -0700

    fix graph service to use settings blockchainAddress key

--- src/app/graph.service.ts
+++ src/app/graph.service.ts
@@ -7,8 +7,8 @@ import { BulletinSecretService } from './bulletinSecret.service';
 export class GraphService {
     graph: any;
     constructor(private storage: Storage, private http: HTTP, private bulletinSecret: BulletinSecretService) {
-        this.storage.get('blockchainurl').then((blockchainurl) => {
-            this.http.get('http://192.168.1.55:5000/get-graph-mobile?bulletin_secret='+bulletinSecret.bulletin_secret, {}, {}).then((res) => {
+        this.storage.get('blockchainAddress').then((blockchainAddress) => {
+            this.http.get(blockchainAddress + '/get-graph-mobile?bulletin_secret='+bulletinSecret.bulletin_secret, {}, {}).then((res) => {
                 this.graph = JSON.parse(res.data);
             });
         });
