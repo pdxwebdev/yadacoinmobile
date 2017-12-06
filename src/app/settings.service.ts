76e23552fc38389c625e31b7a5de2b32dee8e18d
Matt Vogel <matt@portlandwebdev.com>
2017-12-06 00:51:20 -0800

    change default ip address

--- src/app/settings.service.ts
+++ src/app/settings.service.ts
@@ -12,7 +12,7 @@ export class SettingsService {
     constructor(private storage: Storage, private http: HTTP, private bulletinSecret: BulletinSecretService) {
         this.storage.get('baseAddress').then((baseAddress) => {
             if(baseAddress == null) {
-                this.baseAddress = 'http://34.237.46.10';
+                this.baseAddress = 'http://71.237.161.227:5000';
                 this.storage.set('baseAddress', this.baseAddress);
             } else {
                 this.baseAddress = baseAddress;
@@ -20,7 +20,7 @@ export class SettingsService {
         });
         this.storage.get('blockchainAddress').then((blockchainAddress) => {
             if(blockchainAddress == null) {
-                this.blockchainAddress = 'http://34.237.46.10/transaction';
+                this.blockchainAddress = 'http://71.237.161.227:5000/transaction';
                 this.storage.set('blockchainAddress', this.blockchainAddress);
             } else {
                 this.blockchainAddress = blockchainAddress;
@@ -28,7 +28,7 @@ export class SettingsService {
         });
         this.storage.get('graphproviderAddress').then((graphproviderAddress) => {
             if(graphproviderAddress == null) {
-                this.graphproviderAddress = 'http://34.237.46.10/get-graph-mobile';
+                this.graphproviderAddress = 'http://71.237.161.227:5000/get-graph-mobile';
                 this.storage.set('graphproviderAddress', this.graphproviderAddress);
             } else {
                 this.graphproviderAddress = graphproviderAddress;
@@ -36,7 +36,7 @@ export class SettingsService {
         });
         this.storage.get('walletproviderAddress').then((walletproviderAddress) => {
             if(walletproviderAddress == null) {
-                this.walletproviderAddress = 'http://34.237.46.10/wallet';
+                this.walletproviderAddress = 'http://71.237.161.227:5000/wallet';
                 this.storage.set('walletproviderAddress', this.walletproviderAddress);
             } else {
                 this.walletproviderAddress = walletproviderAddress;
